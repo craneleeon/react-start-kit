4d5eabf29fa8e22ec6a39aa417f2018bdc42f21f
Crane Leeon <craneleeon@gmail.com>
2015-08-27 11:39:54 +0800

    20150827

--- grunt/browserify.js
+++ grunt/browserify.js
@@ -3,45 +3,77 @@ var fileset = {
     // 'public/js/bundle.test.js': ['web/app/js/test.js']
 }
 var envify = require('envify/custom');
-module.exports = {
-    dist: {
+var assign = require('object-assign');
+
+function makeConf(env, iswatch){
+    var conf = {
         options: {
             transform: [
-                "reactify",
-                envify({
-                    NODE_ENV: 'production'
-                })
+                ["babelify", { "blacklist": ["regenerator"] }],
+                // "reactify",
+                env
             ]
         },
         files: fileset
-    },
-    build: {
-        options: {
-            transform: [
-                "reactify",
-                envify({
-                    NODE_ENV: 'development'
-                })
-            ]
-        },
-        files: fileset
-    },
-    dev: {
-        options: {
-            transform: [
-                "reactify",
-                envify({
-                    NODE_ENV: 'development'
-                })
-            ],
+    };
+    if(iswatch){
+        conf.options = assign({}, conf.options, {
             watch: true,
             keepAlive: true,
             watchifyOptions: {
                 verbose: true,
                 delay: 600
             }
-        },
-        files: fileset
+        });
     }
+    return conf;
+
+}
+
+module.exports = {
+    dist: makeConf(
+        envify({
+            NODE_ENV: 'production'
+        })
+    ),
+    build: makeConf(
+        envify({
+            NODE_ENV: 'development'
+        })
+    ),
+    dev: makeConf(
+        envify({
+            NODE_ENV: 'development'
+        }),
+        true
+    )
+    // build: {
+    //     options: {
+    //         transform: [
+    //             "reactify",
+    //             envify({
+    //                 NODE_ENV: 'development'
+    //             })
+    //         ]
+    //     },
+    //     files: fileset
+    // },
+    // dev: {
+    //     options: {
+    //         transform: [
+    //             "reactify",
+    //             envify({
+    //                 NODE_ENV: 'development'
+    //             })
+    //         ],
+    //         watch: true,
+    //         keepAlive: true,
+    //         watchifyOptions: {
+    //             verbose: true,
+    //             delay: 600
+    //         }
+    //     },
+    //     files: fileset
+    // }
 
 };
